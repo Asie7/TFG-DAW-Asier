20a1e23327397e76c4dfed4510d9af23267b2429
Asie7 <carlosperaf14@gmail.com>
2025-11-27 07:09:56 +0000

    Vista Landing

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: []
 })
 export class AppComponent {
   title = 'angular-frontend';
