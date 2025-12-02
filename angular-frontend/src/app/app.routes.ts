0946407b46db5c001b09aed1860562b7c2e4e630
Asie7 <carlosperaf14@gmail.com>
2025-12-02 20:33:11 +0000

    Función login

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },           // Página principal
   { path: 'register', component: RegisterComponent },  // /register
+  { path: 'login', component: LoginComponent },        // /login
   { path: '**', redirectTo: '' }                       // Cualquier otra ruta → landing
 ];
