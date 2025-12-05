72258b072b4ad106e3826a65810308400cde1efc
Asie7 <carlosperaf14@gmail.com>
2025-12-05 15:36:12 +0000

    avatar-perfil

--- angular-frontend/src/app/pages/landing/landing.component.ts
+++ angular-frontend/src/app/pages/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserAvatarComponent } from '../../components/user-avatar/user.avatar.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UserAvatarComponent],
   templateUrl: './landing.component.html',
   styles: []
 })
