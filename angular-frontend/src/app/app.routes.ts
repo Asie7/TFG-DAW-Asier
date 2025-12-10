759060977af42ae5efb4c5e97557861d9dd9f76b
Asie7 <carlosperaf14@gmail.com>
2025-12-10 22:08:11 +0000

    Consentimiento cookies y vista política de privacidad

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CalendarsComponent } from './pages/calendars/calendars.component';
 import { CalendarDetailComponent } from './pages/calendar-detail/calendar-detail.component';
+import { PolicyPrivacyComponent } from './pages/policy-privacy/policy-privacy.component'; 
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },        // Página principal
   { path: 'register', component: RegisterComponent }, // /register
   { path: 'login', component: LoginComponent },      // /login
   { path: 'calendars', component: CalendarsComponent }, // /calendars
-  { path: 'calendars/:id', component: CalendarDetailComponent }, // /calendars/:id
+  { path: 'calendars/:id', component: CalendarDetailComponent },
+  { path: 'policy-privacy', component: PolicyPrivacyComponent }, // /calendars/:id
   { path: '**', redirectTo: '' }                     // Cualquier otra ruta → landing
 ];
